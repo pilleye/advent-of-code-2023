7f9951207cd386f433d42b3682d110e4df71d809
Aditya Pillai <github.4nsjt@simplelogin.com>
2023-12-02 09:41:09 -0800

    fix day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -7,8 +7,8 @@ pub fn part_one(input: &str) -> Option<u32> {
         input
             .lines()
             .map(|line| {
-                let first = get_number(line, 0..line.len());
-                let last = get_number(line, (0..line.len()).rev());
+                let first = get_number(line, 0..line.len(), false);
+                let last = get_number(line, (0..line.len()).rev(), false);
                 first * 10 + last
             })
             .sum(),
@@ -30,20 +30,25 @@ static NUMBER_MAPPING: Lazy<Vec<(&str, u32)>> = Lazy::new(|| {
     ]
 });
 
-fn get_number(s: &str, range: impl Iterator<Item = usize>) -> u32 {
+fn get_number(s: &str, range: impl Iterator<Item = usize>, allow_text: bool) -> u32 {
     range
         .map(|i| s.get(i..).unwrap())
-        .map(|s| get_value(s))
+        .map(|s| get_value(s, allow_text))
         .find(|v| v.is_some())
         .unwrap()
         .unwrap()
 }
 
-fn get_value(s: &str) -> Option<u32> {
-    NUMBER_MAPPING
-        .iter()
-        .find_map(|(k, v)| s.starts_with(k).then(|| *v))
-        .or_else(|| s.chars().next().and_then(|c| c.to_digit(10)))
+fn get_value(s: &str, allow_text: bool) -> Option<u32> {
+    s.chars().next().and_then(|c| c.to_digit(10)).or_else(|| {
+        allow_text
+            .then(|| {
+                NUMBER_MAPPING
+                    .iter()
+                    .find_map(|(k, v)| s.starts_with(k).then(|| *v))
+            })
+            .flatten()
+    })
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -51,8 +56,8 @@ pub fn part_two(input: &str) -> Option<u32> {
         input
             .lines()
             .map(|line| {
-                let first = get_number(line, 0..line.len());
-                let last = get_number(line, (0..line.len()).rev());
+                let first = get_number(line, 0..line.len(), true);
+                let last = get_number(line, (0..line.len()).rev(), true);
                 first * 10 + last
             })
             .sum(),
@@ -71,7 +76,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
         assert_eq!(result, Some(281));
     }
 }
